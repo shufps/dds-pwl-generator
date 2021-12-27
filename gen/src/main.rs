d9dd3c7e77af5e9b6b7df57e61d9d613865f41bd
shufps <shufps80@gmail.com>
2021-12-27 09:21:32 +0100

    changed "ADC" to "DAC"

--- gen/src/main.rs
+++ gen/src/main.rs
@@ -4,8 +4,8 @@ use std::io::Write;
 
 const DDS_FREQ : f64 = 1_000.0;         // Hz
 const SAMPLING_RATE : f64 = 44_100.0;   // Hz
-const BITS_ADC : u32 = 12;              // bits
-const VOLTS_ADC : f64 = 3.3;            // V
+const BITS_DAC : u32 = 12;              // bits
+const VOLTS_DAC : f64 = 3.3;            // V
 const DURATION : f64 = 100.0;           // ms
 
 
@@ -26,14 +26,14 @@ fn main() {
 
     let mut w = File::create("dds.pwl").unwrap();
 
-    let mut last = VOLTS_ADC / 2.0;
+    let mut last = VOLTS_DAC / 2.0;
     for i in 1..steps {
         let time = (i as f64 / SAMPLING_RATE * 1_000_000_000.0) as u64; // nanoseconds
         akku += akku_inkr;
         let index = (akku as usize >> 32) & (TABLE_SIZE - 1);
-        let mut data = dds_table[index] * VOLTS_ADC / 2.0 + VOLTS_ADC / 2.0;
+        let mut data = dds_table[index] * VOLTS_DAC / 2.0 + VOLTS_DAC / 2.0;
         // ADC quantisierung
-        let q = VOLTS_ADC / (1<<BITS_ADC) as f64;
+        let q = VOLTS_DAC / (1<<BITS_DAC) as f64;
         data = (data / q).ceil() * q;
         let _ = writeln!(&mut w, "{}n {:.6}", time-1, last);     // creates nice steps
         let _ = writeln!(&mut w, "{}n {:.6}", time, data); 
